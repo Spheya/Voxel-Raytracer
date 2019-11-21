4166552df0f7dc069bdd3956d03ea60938cf7fe5
Spheya <33225496+Spheya@users.noreply.github.com>
2019-11-21 15:11:11 +0100

    Voxel data structure

---
+++ Proftaak/VoxelData/Voxel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxelData
+{
+    struct Voxel
+    {
+        public int ColourValue => (_r << 16) | (_g << 8) | (_b << 0);
+
+        public byte _r, _g, _b;
+
+        public Voxel(int rgb)
+        {
+            _r = (byte)((rgb >> 16) & 0xFF);
+            _g = (byte)((rgb >>  8) & 0xFF);
+            _b = (byte)((rgb >>  0) & 0xFF);
+        }
+
+        public Voxel(byte r = 0xFF, byte g = 0xFF, byte b = 0xFF)
+        {
+            _r = r;
+            _g = g;
+            _b = b;
+        }
+    }
+}
---
+++ Proftaak/VoxelData/VoxelGrid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxelData
+{
+    class VoxelGrid
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public int Depth { get; }
+
+        private readonly Voxel[,,] _voxels;
+
+        public int[] VoxelColours { get; }
+        //TODO: Reflectivity 'n shit
+
+        public Voxel this[int x, int y, int z]
+        {
+            get => _voxels[x, y, z];
+            set {
+                _voxels[x, y, z] = value;
+
+                VoxelColours[x + y * Width + z * Width * Depth] = value.ColourValue;
+            }
+        }
+
+        public VoxelGrid(int width, int height, int depth)
+        {
+            Debug.Assert(width > 0);
+            Debug.Assert(height > 0);
+            Debug.Assert(depth > 0);
+
+            Width = width;
+            Height = height;
+            Depth = depth;
+
+            _voxels = new Voxel[width, height, depth];
+
+            VoxelColours = new int[width * height * depth];
+        }
+    }
+}
