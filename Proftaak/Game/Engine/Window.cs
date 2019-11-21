d4501c7555bd928c1b4af7e83d2e6e51f5b0455a
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-11-21 14:01:43 +0100

    ebic 😎😎🛹🛹

--- Proftaak/Game/Engine/Window.cs
+++ Proftaak/Game/Engine/Window.cs
@@ -5,11 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
 
 namespace Game.Engine
 {
     public sealed class Window : GameWindow
     {
+        public Window() : base(
+                1280,
+                720,
+                GraphicsMode.Default,
+                "Gamer time",
+                GameWindowFlags.Default,
+                DisplayDevice.Default,
+                4,
+                0,
+                GraphicsContextFlags.ForwardCompatible
+            )
+        {
+            Title += ": OpenGL " + GL.GetString(StringName.Version);
+            VSync = VSyncMode.Off;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            CursorVisible = true;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0,0, Width, Height);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            HandleKeyboard();
+        }
+
+        private void HandleKeyboard()
+        {
+            var keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Key.Escape))
+                Exit();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            Title = $"(Vsync: {VSync}) FPS: {1f / e.Time:0}";
+
+            Color4 backColor;
+            backColor.A = 1.0f;
+            backColor.R = 0.1f;
+            backColor.G = 0.1f;
+            backColor.B = 0.3f;
+            GL.ClearColor(backColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
     }
 }
