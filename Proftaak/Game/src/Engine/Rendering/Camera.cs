c4e2016c994d16754d416a616d5303a4a5f4cf8a
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-11-22 13:00:06 +0100

    🐱‍🐉

--- Proftaak/Game/src/Engine/Rendering/Camera.cs
+++ Proftaak/Game/src/Engine/Rendering/Camera.cs
@@ -1,4 +1,4 @@
-﻿using Abacus.SinglePrecision;
+﻿using OpenTK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +10,37 @@ namespace Game.src.Engine.Rendering
     class Camera
     {
         public Vector3 Position;
-        public Vector3 Forward;
+        public Vector3 Rotation;
 
-        public Camera(Vector3 position, Vector3 forward)
+        private Matrix4 RotationMatrix;
+
+        public Camera(Vector3 position, Vector3 rotation)
         {
             Position = position;
-            Forward = forward;
+            Rotation = rotation;
+
+            Matrix4 rotX = new Matrix4(
+                1, 0,                            0,                           0,
+                0, (float)Math.Cos(rotation.X),  (float)Math.Sin(rotation.X), 0,
+                0, (float)-Math.Sin(rotation.X), (float)Math.Cos(rotation.X), 0,
+                0, 0,                            0,                           1
+                );
+
+            Matrix4 rotY = new Matrix4(
+                (float)Math.Cos(rotation.Y),  0, (float)Math.Sin(rotation.Y), 0,
+                0,                            1, 0,                           0,
+                (float)-Math.Sin(rotation.Y), 0, (float)Math.Cos(rotation.Y), 0,
+                0,                            0, 0,                           1
+                );
+            
+            Matrix4 rotZ = new Matrix4(
+                (float)Math.Cos(rotation.Z),  (float)Math.Sin(rotation.Z), 0, 0,
+                (float)-Math.Sin(rotation.Z), (float)Math.Cos(rotation.Z), 0, 0,
+                0,                            0,                           1, 0,
+                0,                            0,                           0, 1
+                );
+
+            RotationMatrix = rotZ * rotY * rotX;
         }
 
         //TODO: Add helper functions here, like setting rotation and such
