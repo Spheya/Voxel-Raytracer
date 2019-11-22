8786fa6145363049db4c41a9a1c1fd190aa5f360
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-11-22 11:28:45 +0100

    hhhhhhhhhhhhhhhhhhhhh

---
+++ Proftaak/Game/src/Engine/Rendering/Camera.cs
@@ -0,0 +1,23 @@
+﻿using Abacus.SinglePrecision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.src.Engine.Rendering
+{
+    class Camera
+    {
+        public Vector3 Position;
+        public Vector3 Forward;
+
+        public Camera(Vector3 position, Vector3 forward)
+        {
+            Position = position;
+            Forward = forward;
+        }
+
+        //TODO: Add helper functions here, like setting rotation and such
+    }
+}
