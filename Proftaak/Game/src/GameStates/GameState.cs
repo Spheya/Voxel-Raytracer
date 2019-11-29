03ec4ed2a61485ec1d7ef9ef762fd33a1b1c9d88
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-11-29 16:15:17 +0100

    COMMIT MY HOMIES

--- Proftaak/Game/src/GameStates/GameState.cs
+++ Proftaak/Game/src/GameStates/GameState.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CsharpVoxReader;
 using Game.Engine.Input;
 using Game.Engine.Maths;
 using Game.Engine.Rendering;
@@ -12,6 +13,7 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Input;
 using VoxelData;
+using VoxLoader;
 
 namespace Game.GameStates
 {
@@ -48,16 +50,24 @@ namespace Game.GameStates
             for (int x = 0; x < 32; x++)
             for (int y = 0; y < 32; y++)
             for (int z = 0; z < 32; z++)
-                _model[x, y, z] = new Voxel((ushort) ((x + y + z) & 1));
+                _model[x, y, z] = (byte)((x+y+z)&1);//new Voxel((ushort) ((x + y + z) & 1));
 
 
-            _model2 = _renderer.CreateModel(32, 32, 32,
+            _model2 = _renderer.CreateModel(72, 126, 72,
                 new Transform(new Vector3(-24.0f, 0.0f, 0.0f), Vector3.Zero, new Vector3(0.5f)));
 
-            for (int x = -16; x < 16; x++)
-            for (int y = -16; y < 16; y++)
-            for (int z = -16; z < 16; z++)
-                _model2[x + 16, y + 16, z + 16] = (x * x + y * y + z * z < 16 * 16) ? new Voxel(1) : Voxel.EMPTY;
+            MyVoxLoader CastleVox = new MyVoxLoader();
+            VoxReader r = new VoxReader(@"res\maps\monu10.vox", CastleVox);
+            r.Read();
+
+            //for (int x = -16; x < 16; x++)
+            //for (int y = -16; y < 16; y++)
+            //for (int z = -16; z < 16; z++)
+            //    _model2[x + 16, y + 16, z + 16] = (x * x + y * y + z * z < 16 * 16) ? (byte)1 : (byte)0;
+            for (int x = 0; x < 72; x++)
+            for (int y = 0; y < 126; y++)
+            for (int z = 0; z < 72; z++)
+                _model2[x,y,z] = CastleVox._data[x,y,z];
 
             Console.WriteLine("Epic");
 
@@ -69,7 +79,7 @@ namespace Game.GameStates
             for (int x = 0; x < s; x++)
             for (int y = 0; y < 1; y++)
             for (int z = 0; z < s; z++)
-                model3[x,y,z] = new Voxel(1);
+                model3[x,y,z] = (byte)1;
 
             Console.WriteLine("Epic");
         }
---
+++ Proftaak/VoxLoader/MyVoxLoader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using CsharpVoxReader;
+using CsharpVoxReader.Chunks;
+using VoxelData;
+
+namespace VoxLoader
+{
+    public class MyVoxLoader : IVoxLoader
+    {
+        public VoxelGrid _data;
+
+        public void LoadModel(int sizeX, int sizeY, int sizeZ, byte[,,] data)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"poggers (model size: {sizeX}; {sizeY}; {sizeZ})");
+            _data = new VoxelGrid(sizeX, sizeY, sizeZ, data);
+        }
+
+        public void LoadPalette(uint[] palette)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void NewGroupNode(int id, Dictionary<string, byte[]> attributes, int[] childrenIds)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void NewLayer(int id, Dictionary<string, byte[]> attributes)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void NewMaterial(int id, Dictionary<string, byte[]> attributes)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void NewShapeNode(int id, Dictionary<string, byte[]> attributes, int[] modelIds, Dictionary<string, byte[]>[] modelsAttributes)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void NewTransformNode(int id, int childNodeId, int layerId, Dictionary<string, byte[]>[] framesAttributes)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SetMaterialOld(int paletteId, MaterialOld.MaterialTypes type, float weight, MaterialOld.PropertyBits property, float normalized)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SetModelCount(int count)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
