52bf95ff41a570c995697179412848b69f3a8474
Spheya <33225496+Spheya@users.noreply.github.com>
2019-12-02 10:48:32 +0100

    Material Struct and crosshair is S P H E R I C A L

---
+++ Proftaak/Game/src/Engine/Rendering/Material.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Engine.Shaders;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Game.Engine.Rendering
+{
+    class Material
+    {
+        public Vector3 BaseColour { get; set; }
+        public float RefractiveIndex { get; set; }
+
+        public Material(Vector3 baseColour, float refractiveIndex = 1.1f)
+        {
+            BaseColour = baseColour;
+            RefractiveIndex = refractiveIndex;
+        }
+        public void Load(ShaderProgram shader, string name)
+        {
+            GL.Uniform3(shader.GetUniformLocation(name + ".baseColour"), BaseColour);
+            GL.Uniform1(shader.GetUniformLocation(name + ".refractiveIndex"), RefractiveIndex);
+        }
+    }
+}
