fc2d3437c594ef7b093eb28febcc9c5a3cdf5186
Spheya <33225496+Spheya@users.noreply.github.com>
2019-12-02 14:10:32 +0100

    \/\/\/\/\/\ <- reflection ray ends here

--- Proftaak/Game/src/Program.cs
+++ Proftaak/Game/src/Program.cs
@@ -8,7 +8,7 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            new Window(new SpashScreenState()).Run();
+            new Window(new GameState()).Run();
         }
     }
 }
