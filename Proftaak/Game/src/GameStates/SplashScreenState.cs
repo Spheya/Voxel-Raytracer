c4d1970b5455d8040205ff38330e5c8a5b7e59da
mike3mike <mikeleijten1@gmail.com>
2019-12-03 14:32:32 +0100

    Mike heeft fade gemaakt.

--- Proftaak/Game/src/GameStates/SplashScreenState.cs
+++ Proftaak/Game/src/GameStates/SplashScreenState.cs
@@ -8,12 +8,15 @@ using Game.Engine.Shaders;
 using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 using Game.Engine.Maths;
+using System.Threading;
 
 namespace Game.GameStates
 {
-    class SpashScreenState : ApplicationState
+    class SplashScreenState : ApplicationState
     {
         SpriteRenderer renderer;
+        Sprite sprite1;
+        Transform transform1;
 
         public override void OnCreate()
         {
@@ -22,23 +25,19 @@ namespace Game.GameStates
                 Shader spriteVertexShader = new Shader(ShaderType.VertexShader, ShaderPreprocessor.Execute(@"res\shaders\ui\vertex.glsl"));
                 Shader spriteFragmentShader = new Shader(ShaderType.FragmentShader, ShaderPreprocessor.Execute(@"res\shaders\ui\fragment.glsl"));
                 renderer = new SpriteRenderer(new ShaderProgram(new[] { spriteVertexShader, spriteFragmentShader }));
+                Vector3 vector1 = new Vector3(20, 30, 30);
+                transform1 = new Transform(vector1, Vector3.Zero, new Vector3(window.Width, window.Height, 30));
+                Texture texture1 = new Texture("res\\textures\\mini_yoda.png");
+                sprite1 = new Sprite(texture1);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 throw;
             }
-            Texture texture;
-            Vector3 vector1 = new Vector3(20, 30, 30);
-            Transform transform1 = new Transform(vector1, Vector3.Zero, new Vector3(System.Windows.SystemParameters.PrimaryScreenWidth, window.Height, 30));
-            float kleur = 0.5F;
-            Colour kleurdit = new Colour(0, 255, 0);
-            Texture texture1 = new Texture("res\\textures\\crosshair.png");
-            Sprite sprite1 = new Sprite(texture1);
-            renderer.Add(sprite1);
-            sprite1.Colour = kleurdit;
-            sprite1.Transform = transform1;
+            //Colour Color2 = new Colour(0, 255, 0, 1f);
         }
+
 
         public override void OnDestroy()
         {
@@ -52,8 +51,20 @@ namespace Game.GameStates
         {
         }
 
+        const float beginTime = 2.0f;
+        const float speedFade = 5.0f;
+        float count = 0;
+        float fade = 1.0f;
         public override void OnUpdate(float deltatime)
         {
-        }
+            count += deltatime;
+            if (count >= beginTime) {
+                fade -= deltatime / speedFade;
+            }
+            Colour Color1 = new Colour(fade, fade, fade);
+            sprite1.Colour = Color1;
+            renderer.Add(sprite1);
+            sprite1.Transform = transform1;
+;        }
     }
 }
