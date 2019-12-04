ed21e713cb8c5eea372be396eaa88b703919ce7a
Spheya <33225496+Spheya@users.noreply.github.com>
2019-12-04 09:51:07 +0100

    Testcode for Luc

--- Proftaak/ArduinoInput/Program.cs
+++ Proftaak/ArduinoInput/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,24 @@ namespace ArduinoInput
 {
     class Program
     {
+        public static void Test()
+        {
+            SerialPort port = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+
+            try
+            {
+                port.Open();
+
+                while (port.BytesToRead > 0)
+                    Console.WriteLine(port.ReadByte());
+
+            }
+            finally
+            {
+                port.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
         }
