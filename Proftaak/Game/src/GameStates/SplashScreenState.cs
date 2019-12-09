e121116dc43bb43901aa723a1db31a2695ab6f03
Spheya <33225496+Spheya@users.noreply.github.com>
2019-12-09 09:45:02 +0100

    Actually usinig splashshcreen now + applied naming conventions

--- Proftaak/Game/src/GameStates/SplashScreenState.cs
+++ Proftaak/Game/src/GameStates/SplashScreenState.cs
@@ -14,9 +14,15 @@ namespace Game.GameStates
 {
     class SplashScreenState : ApplicationState
     {
-        SpriteRenderer renderer;
-        Sprite sprite1;
-        Transform transform1;
+        const float BEGIN_TIME = 0.2f;
+        const float SPEED_FADE = 1.5f;
+
+        float _count = 0;
+        float _fade = 1.0f;
+
+        SpriteRenderer _renderer;
+        Sprite _sprite1;
+        Transform _transform1;
 
         public override void OnCreate()
         {
@@ -24,18 +30,17 @@ namespace Game.GameStates
             {
                 Shader spriteVertexShader = new Shader(ShaderType.VertexShader, ShaderPreprocessor.Execute(@"res\shaders\ui\vertex.glsl"));
                 Shader spriteFragmentShader = new Shader(ShaderType.FragmentShader, ShaderPreprocessor.Execute(@"res\shaders\ui\fragment.glsl"));
-                renderer = new SpriteRenderer(new ShaderProgram(new[] { spriteVertexShader, spriteFragmentShader }));
-                Vector3 vector1 = new Vector3(20, 30, 30);
-                transform1 = new Transform(vector1, Vector3.Zero, new Vector3(window.Width, window.Height, 30));
-                Texture texture1 = new Texture("res\\textures\\mini_yoda.png");
-                sprite1 = new Sprite(texture1);
+                _renderer = new SpriteRenderer(new ShaderProgram(new[] { spriteVertexShader, spriteFragmentShader }));
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 throw;
             }
-            Colour Color2 = new Colour(0, 255, 0, 1f);
+            Vector3 vector1 = new Vector3(20, 30, 30);
+            _transform1 = new Transform(vector1, Vector3.Zero, new Vector3(window.Width, window.Height, 30));
+            Texture texture1 = new Texture("res\\textures\\splashscreen.png");
+            _sprite1 = new Sprite(texture1);
         }
 
 
@@ -44,27 +49,29 @@ namespace Game.GameStates
         }
         public override void OnDraw(float deltatime)
         {
-            renderer.Draw(window);
+            _renderer.Draw(window);
         }
 
         public override void OnFixedUpdate(float deltatime)
         {
         }
 
-        const float beginTime = 2.0f;
-        const float speedFade = 5.0f;
-        float count = 0;
-        float fade = 1.0f;
         public override void OnUpdate(float deltatime)
         {
-            count += deltatime;
-            if (count >= beginTime) {
-                fade -= deltatime / speedFade;
+            _count += deltatime;
+            if (_count >= BEGIN_TIME) {
+                _fade -= deltatime * SPEED_FADE;
+
+                if (_fade <= 0.0f)
+                {
+                    RequestState(new GameState());
+                }
             }
-            Colour Color1 = new Colour(fade, fade, fade);
-            sprite1.Colour = Color1;
-            renderer.Add(sprite1);
-            sprite1.Transform = transform1;
+
+            Colour color1 = new Colour(_fade, _fade, _fade);
+            _sprite1.Colour = color1;
+            _renderer.Add(_sprite1);
+            _sprite1.Transform = _transform1;
 ;        }
     }
 }
