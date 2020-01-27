f61b1617e8ca4825edc82b0feac94e93d1eeb4ff
Luuk van Oijen <lazyluuk.channel@gmail.com>
2020-01-27 02:23:10 +0100

    aaaaaaaaaaaaaah batterij leeg

--- Proftaak/Game/src/Gameplay/Player.cs
+++ Proftaak/Game/src/Gameplay/Player.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntitySystem;
+using Game.Engine.Input;
 using Game.Engine.Maths;
 using Game.Engine.Rendering;
 using Networking;
+using OpenTK;
 
 namespace Game.Gameplay
 {
@@ -18,18 +20,21 @@ namespace Game.Gameplay
     class Player : NetworkEntity<PlayerPacket>
     {
         public VoxelModel _modelBody; //Merely a reference, not enough time to think all this through, so it's public.
-        private Transform transform;
+        private Transform _transform;
         private bool _controllable;
+        private Camera camera;
+
+        private float _speed = 7f;
 
         public Player(ulong id, ulong ownerId, bool controllable) : base(id, ownerId, Type.PLAYER) {
-            transform = new Transform();
-            transform.Position = new OpenTK.Vector3(0.0f, 32.0f, 0.0f);
+            _transform = new Transform();
+            _transform.Position = new OpenTK.Vector3(0.0f, 32.0f, 0.0f);
             _controllable = controllable;
         }
         public override void FixedUpdate(EntityManager entityManager, float deltatime)
         {
             //throw new NotImplementedException();
-            _modelBody.Transform.Position = transform.Position;
+            _modelBody.Transform.Position = _transform.Position;
         }
 
         public override PlayerPacket GetNetworkData()
@@ -40,6 +45,7 @@ namespace Game.Gameplay
         public override void NetworkUpdate(PlayerPacket packet)
         {
             //throw new NotImplementedException();
+            _transform.Position = new OpenTK.Vector3(packet.x, packet.y, packet.z);
         }
 
         public override void OnAdd(EntityManager entityManager)
@@ -58,6 +64,30 @@ namespace Game.Gameplay
             if (_controllable)
             {
                 //Take input and move player
+                bool pressedFD = KeyboardInput.IsForwardDown();
+                bool pressedBD = KeyboardInput.IsBackwardDown();
+                bool pressedSLD = KeyboardInput.IsStrafeLeftDown();
+                bool pressedSRD = KeyboardInput.IsStrafeRightDown();
+
+                if (pressedFD == true)
+                {
+                    _transform.Position += new Vector3(_speed * (float)Math.Sin(_transform.Rotation.Y) * deltatime, 0f, _speed * (float)Math.Cos(_transform.Rotation.Y) * deltatime);
+                }
+                if (pressedBD == true)
+                {
+                    //_transform.Position += new Vector3(1 * deltatime, 0f, 0f);
+                    _transform.Position -= new Vector3(_speed * (float)Math.Sin(_transform.Rotation.Y) * deltatime, 0f, _speed * (float)Math.Cos(_transform.Rotation.Y) * deltatime);
+                }
+                if (pressedSLD == true)
+                {
+                    //_transform.Position += new Vector3(1 * deltatime, 0f, 0f);
+                    _transform.Position -= new Vector3(_speed * (float)Math.Cos(-_transform.Rotation.Y) * deltatime, 0f, _speed * (float)Math.Sin(-_transform.Rotation.Y) * deltatime);
+                }
+                if (pressedSRD == true)
+                {
+                    //_transform.Position += new Vector3(1 * deltatime, 0f, 0f);
+                    _transform.Position += new Vector3(_speed * (float)Math.Cos(-_transform.Rotation.Y) * deltatime, 0f, _speed * (float)Math.Sin(-_transform.Rotation.Y) * deltatime);
+                }
             }
         }
     }
